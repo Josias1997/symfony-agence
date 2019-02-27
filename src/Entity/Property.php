96aad2c1f177454b506c878ce8de14db9c1f5662
Kologo Boinzemwinde Josias Yannick <bjykologo@SQLI-BJYKOLOGO.SQLI>
2019-02-27 09:58:31 +0100

    validation des données

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -29,6 +32,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=5, max=255)
      */
     private $title;
 
@@ -39,6 +43,7 @@ class Property
 
 /**                                                 
      * @ORM\Column(type="integer")
+     * @Assert\Range(min=10, max=400)
      */
     private $area;
 
@@ -74,6 +79,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(pattern="/^[0-9]{5}$/")
      */
     private $postal_code;
 
